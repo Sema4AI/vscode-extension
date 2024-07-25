536472813482f16bb1e0a6791b25c4b385d6cfcc
Michal Huras <michal@sema4.ai>
2024-07-25 11:46:06 +0200

    test fix

--- sema4ai/tests/sema4ai_code_tests/test_agent_server.py
+++ sema4ai/tests/sema4ai_code_tests/test_agent_server.py
@@ -3,15 +3,15 @@ import os
 
 def test_agent_server_download() -> None:
     from sema4ai_code.agent_server import (
-        get_default_agent_server_location,
         download_agent_server,
     )
 
-    agent_server_location = get_default_agent_server_location()
+    from sema4ai_code import get_bin_folder
 
-    if os.path.exists(agent_server_location):
-        os.remove(agent_server_location)
+    tmp_agent_server_location = f"{get_bin_folder()}/agent-server-tmp"
 
-    download_agent_server(agent_server_location)
+    download_agent_server(tmp_agent_server_location)
 
-    assert os.path.exists(agent_server_location)
+    assert os.path.exists(tmp_agent_server_location)
+
+    os.remove(tmp_agent_server_location)
