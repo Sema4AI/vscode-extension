76b3b9fe0f95165eed2847de100e5dace5ade54c
Ovidiu Rusu <ovidiu@sema4.ai>
2024-08-21 16:01:11 +0300

    Lint

--- sema4ai/tests/sema4ai_code_tests/test_vscode_integration.py
+++ sema4ai/tests/sema4ai_code_tests/test_vscode_integration.py
@@ -9,17 +9,6 @@ from typing import Dict, List, Optional
 from unittest import mock
 
 import pytest
-from sema4ai_code_tests.fixtures import RccPatch
-from sema4ai_code_tests.protocols import IRobocorpLanguageServerClient
-from sema4ai_ls_core.basic import wait_for_condition
-from sema4ai_ls_core.callbacks import Callback
-from sema4ai_ls_core.ep_resolve_interpreter import (
-    DefaultInterpreterInfo,
-    IInterpreterInfo,
-)
-from sema4ai_ls_core.pluginmanager import PluginManager
-from sema4ai_ls_core.unittest_tools.cases_fixture import CasesFixture
-
 from sema4ai_code.inspector.common import (
     STATE_CLOSED,
     STATE_NOT_PICKING,
@@ -32,6 +21,17 @@ from sema4ai_code.protocols import (
     LocalPackageMetadataInfoDict,
     WorkspaceInfoDict,
 )
+from sema4ai_ls_core.basic import wait_for_condition
+from sema4ai_ls_core.callbacks import Callback
+from sema4ai_ls_core.ep_resolve_interpreter import (
+    DefaultInterpreterInfo,
+    IInterpreterInfo,
+)
+from sema4ai_ls_core.pluginmanager import PluginManager
+from sema4ai_ls_core.unittest_tools.cases_fixture import CasesFixture
+
+from sema4ai_code_tests.fixtures import RccPatch
+from sema4ai_code_tests.protocols import IRobocorpLanguageServerClient
 
 log = logging.getLogger(__name__)
 
@@ -1072,9 +1072,10 @@ def test_hover_image_integration(
 ):
     import base64
 
-    from sema4ai_code_tests.fixtures import IMAGE_IN_BASE64
     from sema4ai_ls_core import uris
     from sema4ai_ls_core.workspace import Document
+
+    from sema4ai_code_tests.fixtures import IMAGE_IN_BASE64
 
     locators_json = tmpdir.join("locators.json")
     locators_json.write_text("", "utf-8")
@@ -1345,14 +1346,13 @@ def test_profile_import(
     datadir,
     disable_rcc_diagnostics,
 ):
-    from sema4ai_ls_core import uris
-
     from sema4ai_code.commands import (
         SEMA4AI_GET_PY_PI_BASE_URLS_INTERNAL,
         SEMA4AI_PROFILE_IMPORT_INTERNAL,
         SEMA4AI_PROFILE_LIST_INTERNAL,
         SEMA4AI_PROFILE_SWITCH_INTERNAL,
     )
+    from sema4ai_ls_core import uris
 
     result = language_server_initialized.execute_command(
         SEMA4AI_GET_PY_PI_BASE_URLS_INTERNAL,
@@ -1630,10 +1630,11 @@ def test_web_inspector_integrated(
     This test should be a reference spanning all the APIs that are available
     for the inspector webview to use.
     """
+    from sema4ai_ls_core import uris
+
     from sema4ai_code_tests.robocode_language_server_client import (
         RobocorpLanguageServerClient,
     )
-    from sema4ai_ls_core import uris
 
     cases.copy_to("robots", ws_root_path)
     ls_client: RobocorpLanguageServerClient = language_server_initialized
@@ -2384,7 +2385,7 @@ def test_create_and_pack_agent_package(
     assert "not empty" in result["message"]
 
     # Correct agent spec configuration
-    agent_spec = {
+    agent_spec: dict = {
         "agent-package": {
             "spec-version": "v1",
             "agents": [
@@ -2418,4 +2419,7 @@ def test_create_and_pack_agent_package(
     )
 
     assert not result["success"]
-    assert result["message"] == "Error validating the agent package:\nLine 3: 'description' section not found."
+    assert (
+        result["message"]
+        == "Error validating the agent package:\nLine 3: 'description' section not found."
+    )
