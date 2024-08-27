41224168b6b5673248ecf4cfe9e542d81b402ccc
Fabio Zadrozny <fabiofz@gmail.com>
2024-08-27 15:52:23 -0300

    wip-6

--- sema4ai/vscode-client/src/rcc.ts
+++ sema4ai/vscode-client/src/rcc.ts
@@ -154,9 +154,9 @@ async function downloadRcc(
 
 // Note: python tests scan this file and get these constants, so, if the format
 // changes the (failing) test also needs to change.
-const BASENAME_PREBUILT_WIN_AMD64 = "b056d4679fd5c75e_windows_amd64.zip";
-const BASENAME_PREBUILT_DARWIN = "c6678e05ba2c7c03_darwin_amd64.zip";
-const BASENAME_PREBUILT_LINUX_AMD64 = "23c94aa3947b8e5b_linux_amd64.zip";
+const BASENAME_PREBUILT_WIN_AMD64 = "1d28643429842f9e_windows_amd64.zip";
+const BASENAME_PREBUILT_DARWIN = "91aa29443ed4aaf5_darwin_amd64.zip";
+const BASENAME_PREBUILT_LINUX_AMD64 = "ae6280a15fc287de_linux_amd64.zip";
 
 function getBaseAsZipBasename() {
     let basename: string;
