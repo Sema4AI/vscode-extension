e2e7756e7b5542fcb83ad686120c2a97ba6259a4
Fabio Zadrozny <fabiofz@gmail.com>
2024-12-02 11:40:37 -0300

    Verify version of sema4.ai data extension.

--- sema4ai/vscode-client/src/dataExtension.ts
+++ sema4ai/vscode-client/src/dataExtension.ts
@@ -15,7 +15,9 @@ function isDataExtensionInstalled() {
         if (extension) {
             return true;
         }
-    } catch (error) {}
+    } catch (error) {
+        logError("Error checking if data extension is installed", error, "ERR_CHECK_DATA_EXTENSION_INSTALLED");
+    }
 
     return false;
 }
@@ -27,7 +29,34 @@ export async function verifyDataExtensionIsInstalled(
     msg: string = "It seems that the Sema4AI data extension is not installed. Do you wish to install it?"
 ) {
     if (isDataExtensionInstalled()) {
-        return true;
+        try {
+            let extension = extensions.getExtension(DATA_EXTENSION_ID);
+            let version = extension?.packageJSON.version;
+            if (version) {
+                // If we have a '-' in the version, remove it and everything after it
+                if (version.includes("-")) {
+                    version = version.split("-")[0];
+                }
+            }
+            // Check if version matches something as <number>.<number>.<number>
+            if (version && version.match(/^\d+\.\d+\.\d+$/)) {
+                // Check if the version is greater or equal to 1.0.2 (use regexp to extract the version number)
+                let [major, minor, patch] = version.split(".").map(Number);
+                if (major > 1 || (major == 1 && minor > 0) || (major == 1 && minor == 0 && patch >= 2)) {
+                    return true;
+                }
+            }
+
+            // Notify users that the version is too old (or it didn't match the regexp)
+            window.showWarningMessage(
+                `The Sema4AI data extension is installed, but the version (${version}) is too old (minimum required version is 1.0.2). Please update it.`
+            );
+            commands.executeCommand("workbench.extensions.search", DATA_EXTENSION_ID);
+            return false;
+        } catch (error) {
+            logError("Error checking data extension version", error, "ERR_CHECK_DATA_EXTENSION_VERSION");
+            return false;
+        }
     }
 
     let install = "Install";
@@ -37,6 +66,7 @@ export async function verifyDataExtensionIsInstalled(
         try {
             await commands.executeCommand("workbench.extensions.installExtension", DATA_EXTENSION_ID);
         } catch (error) {
+            commands.executeCommand("workbench.extensions.search", DATA_EXTENSION_ID);
             logError("Error installing data extension", error, "ERR_INSTALL_DATA_EXTENSION");
             window.showErrorMessage(
                 "Unable to install the Sema4AI data extension. Please install it manually and retry."
