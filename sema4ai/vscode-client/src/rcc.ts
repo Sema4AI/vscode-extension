b3f434706f4a228e6b578f53cb58ff0e794f30c5
Ovidiu Rusu <ovidiu@sema4.ai>
2025-07-22 12:46:48 +0300

    Change prebuild envs

--- sema4ai/vscode-client/src/rcc.ts
+++ sema4ai/vscode-client/src/rcc.ts
@@ -156,10 +156,10 @@ async function downloadRcc(
 
 // Note: python tests scan this file and get these constants, so, if the format
 // changes the (failing) test also needs to change.
-const BASENAME_PREBUILT_WIN_AMD64 = "a2eefcb6eb765393-windows_amd64.zip";
-const BASENAME_PREBUILT_LINUX_AMD64 = "2d449af3bd80289b-linux_amd64.zip";
-const BASENAME_PREBUILT_DARWIN_AMD64 = "3ab0a4536f872f2e-darwin_amd64.zip";
-const BASENAME_PREBUILT_DARWIN_ARM64 = "3ab0a4536f872f2e-darwin_arm64.zip";
+const BASENAME_PREBUILT_WIN_AMD64 = "011f0a205c188a1e-windows_amd64.zip";
+const BASENAME_PREBUILT_LINUX_AMD64 = "c015442cb903ac21-linux_amd64.zip";
+const BASENAME_PREBUILT_DARWIN_AMD64 = "a699cd95e272c43f-darwin_amd64.zip";
+const BASENAME_PREBUILT_DARWIN_ARM64 = "a699cd95e272c43f-darwin_arm64.zip";
 
 function getBaseAsZipBasename() {
     let basename: string;
